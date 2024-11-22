f86bd028f88b1c652958c80d93cfec2228ec8eb8
MartinSebastianMP <87109948+MartinSMP@users.noreply.github.com>
2024-11-21 23:30:04 -0500

    Create citas.js

---
+++ js/citas.js
@@ -0,0 +1,42 @@
+let citas = [
+    {
+        'autor': '- Ernestine Ulmer',
+        'texto': 'La vida es incierta, cómete el postre primero.'
+    },
+    {
+        'autor': '- Edward Lee',
+        'texto': 'Los postres son como una canción que te hace sentir bien y los mejores incluso te hacen bailar.'
+    },
+    {
+        'autor': '- Terri Guillemets',
+        'texto': 'Los postres son el cuento de hadas de la cocina, un final feliz luego de la cena.'
+    },
+    {
+        'autor': '- Fernand Point',
+        'texto': 'Una comida sin postre es como un traje sin corbata.'
+    },
+    {
+        'autor': '- Jim Davis',
+        'texto': 'Los vegetales son imprescindibles en una dieta. Sugiero el pastel de zanahoria y el pay de manzana.'
+    },
+    {
+        'autor': '- Ellen Rose',
+        'texto': 'Las tortas son como los libros: hay libros nuevos que quieres leer y hay otros viejos que quieres volver a leer.'
+    },
+    {
+        'autor': '- Buddy Valastro',
+        'texto': 'Las tortas son especiales. Cada cumpleaños, cada celebración termina con algo dulce: una torta que la gente recordará.'
+    },
+    {
+        'autor': '- Julia Child',
+        'texto': 'Una fiesta sin pastel es solo una reunión.'
+    },
+    {
+        'autor': '- Alain Ducasse',
+        'texto': 'En cualquier lugar del mundo hay tensiones económicas, políticas y religiosas. Lo que necesitamos es chocolate.'
+    },
+    {
+        'autor': '- Elon Musk',
+        'texto': 'Si estás intentando crear una empresa, es como hornear un pastel. Tienes que tener todos los ingredientes en la proporción correcta.'
+    }
+];
