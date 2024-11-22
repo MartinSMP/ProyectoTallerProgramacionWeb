ab3c4f19e0166e7ee7c0d6ad7c4fd594593275f9
MartinSebastianMP <87109948+MartinSMP@users.noreply.github.com>
2024-11-21 23:30:19 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Seleccionar los elementos HTML.
+let botonElem = document.getElementById('boton-cambiar-cita');
+let citaElem = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+function generarEnteroAleatorio(minimo, maximo) {
+  minimo = Math.ceil(minimo);
+  maximo = Math.floor(maximo);
+
+  return Math.floor(Math.random() * (maximo - minimo) + minimo);
+}
+
+// Seleccionar una cita aleatoria.
+function cambiarCita() {
+  let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+  citaElem.textContent = `"${citas[indiceAleatorio].texto}"`;
+  autorElem.textContent = citas[indiceAleatorio].autor;
+}
+
+// Para seleccionar una cita de forma aleatoria
+// cuando se inicia la aplicacion.
+let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+cambiarCita();
+
+// Para cambiar la cita al hacer click en el boton.
+botonElem.addEventListener('click', cambiarCita);
